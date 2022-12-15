ee2cf238fd3e80c7182b409560f7cbc71293a95a
KarinaKrinitskaya <77931318+KarinaKrinitskaya@users.noreply.github.com>
2022-12-15 21:07:36 +0100

    Epam site practice tests. Selenium

---
+++ Selenium_Basics_2/Selenium_Basics_2/EpamSiteTest.cs
@@ -0,0 +1,73 @@
+﻿namespace Selenium_Basics_2;
+
+public class EpamSiteTest
+{
+    private IWebDriver driver;
+    private const string urlMain = "https://www.epam.com/";
+
+    [SetUp]
+    public void Setup()
+    {
+        driver = new ChromeDriver();
+        driver.Manage().Window.Maximize();
+        driver.Navigate().GoToUrl(urlMain);
+        driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(9);
+    }
+
+    [Test]
+    public void CheckListAvailableCountries()
+    {
+        List<string> webElemList = new List<string>() { "AMERICAS", "EMEA", "APAC" };
+        driver.FindElement(By.XPath("//*[@href='/careers']")).Click();
+        var actualCountriesList = driver.FindElements(By.XPath("//*[@class='tabs__title' and contains(@role,'presentation')]")).Select(item => item.Text);
+
+        Assert.That(actualCountriesList, Is.EqualTo(webElemList), "List of countries doesn't have: Americas, EMEA, APAC");
+    }
+
+    [Test]
+    public void CheckSearchInputAutomation()
+    {
+        string urlSearch = "https://www.epam.com/search?q=Automation";
+
+        driver.FindElement(By.XPath("//*[@class='header-search-ui header__control']")).Click();
+        driver.FindElement(By.Id("new_form_search")).SendKeys("Automation");
+        driver.FindElement(By.XPath("//form[@action='/search']/child::button[@class='header-search__submit']")).Click();
+
+        Assert.That(driver.Url, Is.EqualTo(urlSearch), "Search doesn't work!");
+        bool isContains = driver.PageSource.Contains("Automation");
+
+        Assert.IsTrue(isContains,"Text was not found");
+    }
+
+    [Test]
+    public void CheckSearchInputBusinessAnalysis()
+    {
+        string urlSearch = "https://www.epam.com/search?q=Business+Analysis";
+
+        driver.FindElement(By.XPath("//*[@class='header-search-ui header__control']")).Click();
+        driver.FindElement(By.Id("new_form_search")).SendKeys("Business Analysis");
+        driver.FindElement(By.XPath("//form[@action='/search']/child::button[@class='header-search__submit']")).Click();
+
+        Assert.That(driver.Url, Is.EqualTo(urlSearch), "Search doesn't work!");
+        var expRes = driver.FindElement(By.XPath("(//*[@class='search-results__title-link'])[1]")).Text;
+        driver.FindElement(By.XPath("(//*[@class='search-results__title-link'])[1]")).Click();
+        var actualRes = driver.FindElement(By.XPath("//*[@class='title__bottom-line']")).Text;
+
+        Assert.That(expRes, Is.EqualTo(actualRes), "The title of the article does not match the title from the search!");
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        driver.Quit();
+    }
+}
+
+//Task 5
+//Keyword contains - //*[@class='tabs__title' and contains(@role,'presentation')]
+//axis following-sibling/preceding-sibling - //*[@class="button-ui bg-color-white standard"]//following-sibling::span / //*[@class="top-navigation__item continuum"]//preceding-sibling::a
+//axis parent - //*[@class="tabs__link js-tabs-link"]//parent::div
+
+//Site:https://www.epam.com/search?q=Business+Analysis
+//axis first/last - //*[@class='search-results__title-link'])[1] / (//*[@class='search-results__title-link'])[last()]
+//axis child - //form[@action='/search']/child::button[@class='header-search__submit']

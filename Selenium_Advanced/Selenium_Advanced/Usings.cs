f438b5a8aeb427366a86f3e37fd080d909e9e4cc
KarinaKrinitskaya <77931318+KarinaKrinitskaya@users.noreply.github.com>
2022-12-20 04:54:07 +0100

    Epam Site Test Advanced

---
+++ Selenium_Advanced/Selenium_Advanced/Usings.cs
@@ -0,0 +1,7 @@
+﻿global using NUnit.Framework;
+global using System;
+global using OpenQA.Selenium;
+global using OpenQA.Selenium.Chrome;
+global using OpenQA.Selenium.Interactions;
+global using OpenQA.Selenium.Support.UI;
+global using System.Xml.Linq;
